1e48055af61f0903a86248a027ea1892e70d5ee2
Oleksandr Ismailov <tama.rilla@protonmail.com>
2023-12-15 00:29:11 +0100

    Added draft full implementation for wiring pi spi lcd driving.
    Signed-off-by: Oleksandr Ismailov <tama.rilla@protonmail.com>

--- main.cpp
+++ main.cpp
@@ -3,17 +3,25 @@
 #include <wiringPi.h>
 #include <wiringPiSpi.h>
 #include <iostream>
+#include <thread>
 
 class TestSPI : public ISPI {
 public:
   TestSPI() {
-    wiringPiSetup();			// Setup the library
-    pinMode(0, OUTPUT);
-    pinMode(1, OUTPUT);
-    pinMode(1, OUTPUT);
+    wiringPiSetup();
+    pinMode(22, OUTPUT);
+    digitalWrite(22, LOW);
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    digitalWrite(22, HIGH);
+
+    pinMode(24, OUTPUT);
+    digitalWrite(24, HIGH);
+    pinMode(18, OUTPUT);
+    digitalWrite(18, LOW);
     if (wiringPiSPISetup(0, 10000000) == -1) {
       throw std::runtime_error("Could not initialize wiringPi");
     }
+    system("sudo ./wiringOP/gpio/gpio readall");
 //    spi_TFT->begin(sclk, miso, mosi, -1);
 //    spi_TFT->setFrequency(_freq);
 //
@@ -32,15 +40,15 @@ public:
   }
   void chipSelect(bool enable) override {
     printf("%s", enable ? "~CS " : "\nCS \n");
-    digitalWrite(0, !enable);
+    digitalWrite(24, !enable ? HIGH : LOW);
   }
   void commandSelect(bool enable) override {
     printf("%s", enable ? "\nCMD " : "DAT ");
-    digitalWrite(0, enable);
+    digitalWrite(18, enable ? HIGH : LOW);
   }
   auto exchange(uint8_t data) -> uint8_t override {
     printf("0x%02X ", data);
-    int ret = wiringPiSPIDataRW(0, &data, 1);
+    wiringPiSPIDataRW(0, &data, 1);
     return data;
   }
   void sendBuff(uint8_t* data, uint32_t size) override {
@@ -48,7 +56,7 @@ public:
       printf("0x%02X ", data[i]);
     }
     printf("\n");
-    int ret = wiringPiSPIDataRW(0, &data, size);
+    wiringPiSPIDataRW(0, data, size);
   }
 };
 
@@ -56,7 +64,12 @@ int main()
 {
   auto testSpi = TestSPI();
   auto ili9486 = ILI9486(testSpi);
-  ili9486.fillScreen(0xFFFF);
-  ili9486.fillScreen(0x0000);
+  for(int i = 0; i < 10; i++)
+  {
+    ili9486.fillScreen(0xFFFF);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    ili9486.fillScreen(0x0000);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
   return 0;
 }
